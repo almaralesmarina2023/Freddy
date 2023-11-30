09d1a0ff05f96fed0908cc9d29a0eaf1ecf45b65
¨lock04¨ <luisfreddys75@gmail.com>
2023-12-01 00:38:44 +0100

    Implementacion TS de Detalles Receta

--- src/app/detalles-recetas/detalles-recetas.component.ts
+++ src/app/detalles-recetas/detalles-recetas.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Recetas } from '../listar-recetas/recetas.model';
+import { DsRecetasService } from '../ds-recetas.service';
 
 @Component({
   selector: 'app-detalles-recetas',
@@ -8,9 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetallesRecetasComponent {
 indice:number=0;
-
-constructor(private route:ActivatedRoute)
+d_nombre:string="";
+d_metodo:string="";
+d_Ingredientes:string[]=[];
+d_Img:string="";
+d_Receta:Recetas=new Recetas;
+constructor(private route:ActivatedRoute, private ds_servicio:DsRecetasService)
 {
   this.indice=this.route.snapshot.params['id'];
+  this.d_Receta=this.ds_servicio.obtener_Receta(this.indice);
+  this.d_nombre=this.d_Receta.nombre;
+  this.d_metodo=this.d_Receta.metodo_preparacion;
+  this.d_Img=this.d_Receta.Img;
+  this.d_Ingredientes=this.d_Receta.ingredientes;
 } 
 }
